d158ef492b2f15a0abfe764c4ef891fb38d78c92
github username <github email address>
2020-02-11 18:10:33 -0600

    passengers work

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,6 +1,29 @@
 class PassengersController < ApplicationController
 
+    def new
+        @errors = flash[:errors]
+        @passenger = Passenger.new 
+    end
+
+    def create
+        # byebug
+        @passenger = Passenger.new(passenger_params)
+        if @passenger.valid?
+            @passenger.save
+            redirect_to passenger_path(@passenger)
+        else
+            redirect_to new_passenger_path
+            flash[:errors] = @passenger.errors.full_messages
+        end
+    end
+
+
     private
+
+    def passenger_params
+        params.require(:passenger).permit(:first_name, :last_name, :email, :password)
+    end
+
     def set_passenger
         @passenger = Passenger.find(params[:id])
     end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,9 +1,10 @@
 class Passenger < ApplicationRecord
+    has_secure_password
     has_many :reservations 
     has_many :routes, through: :reservations 
 
     validates_presence_of :email
     validates_uniqueness_of :email
-    validates_presence_of :password_digest
+    # validates_presence_of :password_digest {message: "Password required"}
     
 end
