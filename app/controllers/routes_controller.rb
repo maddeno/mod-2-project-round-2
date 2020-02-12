7782076c044b14187f9c30bbe3c00871001dd85b
Niko Dixon <nikodixon@gmail.com>
2020-02-11 18:06:35 -0600

    Created preliminary index and show pages for Routes with appropriate methods.

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,2 +1,18 @@
 class RoutesController < ApplicationController
+    before_action :find_route, only: [:show]
+
+    def index
+        @routes = Route.all  
+    end 
+
+
+    def show
+         
+    end 
+
+    private 
+
+    def find_route 
+       @route = Route.find(params[:id])
+    end 
 end
