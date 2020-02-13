1d1d38e3aea23a00801ba3a51bc5d44ff34601ea
github username <github email address>
2020-02-12 18:10:38 -0600

    Lots of stuff - yay
    exit

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -9,6 +9,7 @@ class PassengersController < ApplicationController
         @passenger = Passenger.new(passenger_params)
         if @passenger.valid?
             @passenger.save
+            session[:passenger_id] = @passenger.id
             redirect_to passenger_path(@passenger)
         else
             redirect_to new_passenger_path
