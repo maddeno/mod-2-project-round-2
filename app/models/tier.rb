39b6fcc30d51aa7968cd07a637104cefb92d12a3
maxmcfadden88 <maxmcfadden88@gmail.com>
2020-02-13 18:49:10 -0600

    max edits

--- app/models/tier.rb
+++ app/models/tier.rb
@@ -1,14 +1,4 @@
 class Tier < ApplicationRecord
     has_many :reservations 
-
-    def tier_counter
-        count = 0
-        Reservation.all.each do |t|
-            if t.tier_id == self.id
-            count +=1
-            end
-        end
-        count
-    end
     
 end
