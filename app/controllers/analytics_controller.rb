a81b57354ac01e97ebce73e88ade1df161240e0a
github username <github email address>
2020-02-14 11:59:26 -0600

    edited analytics page

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -6,7 +6,6 @@ class AnalyticsController < ApplicationController
         @reservation_hash = @reservations.group_by{|reservation| reservation.destination_city}
         @stops = Stop.all.sort_by{|stop| -stop.route.reservations.count}
         @tiers = Tier.all.sort_by{|tier| -tier.reservations.count}
-        
     end 
 
 
