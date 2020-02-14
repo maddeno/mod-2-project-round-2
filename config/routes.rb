cbfcaa5675826bb234a3b5009bed5f657787bf9c
github username <github email address>
2020-02-14 11:39:50 -0600

    added some links

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  get '/analytics', to: 'analytics#index', :as => 'analytics'
   
   resources :stops
   resources :tiers
   resources :reservations
   resources :routes
   resources :passengers
-  resources :analytics
+  # resources :analytics
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
